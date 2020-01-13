cf329c8fa617ec82eb4fcbd58e1016cee7e62020
edward <edward.barrow@platosys.co.uk>
2020-01-13 17:52:36 +0000

    with voucherID ++

--- app/src/main/java/uk/co/platosys/vouch/Store.java
+++ app/src/main/java/uk/co/platosys/vouch/Store.java
@@ -14,32 +14,84 @@ public interface Store extends LockStore {
      * @param voucher
      */
  /**
-  *
+  * Stores the Voucher and returns a Signature, being the Store's
+  * signature of the Voucher
   * @param voucher
   * @return
   */
  Signature store (Voucher voucher);
 
  /**
-  *
+  * Stores the Profile.
   * @param profile
   * @return
   */
      Signature store(Profile profile);
 
  /**
-  *
+  * Stores the Group and its associated list of members.
   * @param group
   * @return
   */
  Signature store (Group group);
+
+    /**
+     *  Retrieves a voucher with the given VoucherID
+     * @param voucherID
+     * @return
+     * @throws VoucherNotFoundException
+     */
      Voucher getVoucher(VoucherID voucherID) throws VoucherNotFoundException;
-     Profile getProfile(VoucherID voucherID) throws VoucherNotFoundException;
-     Group getGroup(VoucherID voucherID) throws VoucherNotFoundException;
+
+    /**
+     * Retrieves a Profile with the given VoucherID
+     * @param voucherID
+     * @return
+     * @throws VoucherNotFoundException
+     */
+    Profile getProfile(VoucherID voucherID) throws VoucherNotFoundException;
+
+    /**
+     * Retrieves the Group with the given ID
+      * @param voucherID
+     * @return
+     * @throws VoucherNotFoundException
+     */
+    Group getGroup(VoucherID voucherID) throws VoucherNotFoundException;
+
+    /**
+     * Returns a List of Profiles having the given name. Often there will only be one, but Profile names are
+     * not guaranteed to be unique.
+     * @param name
+     * @return
+     */
      List<Profile> getProfiles(String name);
+
+    /**
+     * Returns a List of Groups having the given name.
+     * @param name
+     * @return
+     */
      List<Group> getGroups(String name);
+
+    /**
+     * Returns the Vouchers matching the given search terms.
+     * @param searchTerms
+     * @return
+     */
      List<Voucher> findVouchers(String[] searchTerms);
+
+    /**
+     * Returns a list of Signatures of the supplied VoucherID
+     * @param voucherID
+     * @return
+     */
      List<Signature> getSignatures(VoucherID voucherID);
-     Lock getLock();
+
+    /**
+     * Returns the Lock with which this Store signs the Vouchers it stores.
+     * @return
+     */
+    Lock getLock();
 
 }
